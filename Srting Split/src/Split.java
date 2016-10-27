c71ce0a5e922c0aa2f7424ea600ea6470161061d
bellalr21 <17leerubioi79@millshighsch.org>
2016-10-27 14:16:15 -0700

    APCS1 Afterschool 10/27

---
+++ Srting Split/src/Split.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Split 
+{
+
+	public static void main(String[] args) 
+	{
+		//String.split();
+		//It's a method that acts on a string, <StringName>.split(<String sp>);
+		//Where sp is the string where the string splits
+		//And it returns an array
+		// Example: "I like apples!".split(" "); 
+		//		it will split at spaces and return an array of ["I","like","apples!"]
+		// Example 2: "I really like really red apples"split("really")
+		//		it will split at the word "really" and return an array of ["I "," like "," apples!"]
+		
+		//play around with String.split! what happens if you "I reallyreally like apples".split("really") ?
+		
+		
+		//Your task:
+		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
+		 * use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
+		 * What if it's a fancy sandwich with multiple pieces of bread?
+		*/
+		
+		//Asks user for ingredients
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please list ingredients for a sandwich.");
+		String ingredients = userInput.nextLine();
+		
+		//check index of bread
+		
+		//Splits list of ingredients to get middle of sandwich
+		String [] newList = ingredients.split("bread");
+		System.out.println((newList.length));
+		for (int i = 0; i < newList.length; i++){
+			System.out.print(newList [i]);
+			
+		}
+		}
+		
+		//Your task pt 2:
+		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
+		 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
+		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
+		*/
+
+		
+
+	}
+
